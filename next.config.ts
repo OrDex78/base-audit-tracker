c3a9cbf96733599e5ceabbceef0ab1e122faebae
OrDex78 <gs3658362@gmail.com>
2026-04-27 18:31:20 +0530

    fix: switch to turbopack config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      "@react-native-async-storage/async-storage": false,
-      "pino-pretty": false,
-    };
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
